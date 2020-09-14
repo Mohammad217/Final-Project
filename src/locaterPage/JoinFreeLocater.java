29e6298e866437f755ad6615786dd3be0b55d8b3
alaud <alaud@Mohammad-uddin>
2020-09-13 23:23:20 -0400

    h

---
+++ src/locaterPage/JoinFreeLocater.java
@@ -0,0 +1,27 @@
+package locaterPage;
+
+public class JoinFreeLocater {
+	public String joinInLinkLoc 		="/html/body/nav[1]/div/ul/li[1]/a/span"; 
+	public String companyNameLoc        ="//*[@id='company']";
+	public String countryClickLoc       ="#country";//Css
+	public String countrySelectLoc      ="Bangladesh";
+	public String districtClickLoc      ="//*[@id='district_id']";
+	public String districtSelectLoc     ="Cumilla";
+	public String contactPrasonClick    ="body > section > div > div > div.mb-5 > div > div:nth-child(2) > div > form > div.my-5 > div:nth-child(4) > div.col-4.col-sm-3 > select";//Css
+	public String contactselectLoc      ="Mr.";
+	public String fullNameLoc           ="//*[@id='name']";
+	public String phoneNumberTypeLoc    ="//*[@id='phone']";
+	public String emailTypeLoc          ="//*[@id='email']";
+	public String passwordLoc           ="//*[@id='password']";
+	public String confirmPasswordLoc    ="//*[@id='confirm_password']";
+	public String clickSubmitLoc        ="/html/body/section/div/div/div[1]/div/div[2]/div/form/div[2]/button";
+	public String fullNameValue         ="Mohammad uddin";
+	public String phoneNumberValue      ="01716396556";
+	public String emailValue            ="alauddinripon2172007@gmail.com";
+	public String PasswordValue         ="Armansamil08";
+	public String confirmPasswordValue  ="Armansamil08";
+	public String companyNameValue      ="4sonlineStore";
+	public String myActualFullNameLoc   = "Mohammad uddin";
+	
+
+}
