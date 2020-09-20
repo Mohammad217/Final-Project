4222a49efb24f28b49409317490917e3ed93d84e
Mohammad217 <alauddinripon@gmail.com>
2020-09-20 13:15:50 -0400

    test

--- src/testSide/SignInWithInvalideEmail.java
+++ src/testSide/SignInWithInvalideEmail.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package testSide;
 
 import org.openqa.selenium.By;
@@ -25,32 +24,5 @@ public class SignInWithInvalideEmail extends BizBangladesh {
 		 String exp = "Mohammad uddin";
 		 Assert.assertEquals(act, exp);
 }
-=======
-package testSide;
 
-import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import locaterPage.JoinFreeSignIn;
-import mainpage.BizBangladesh;
-
-public class SignInWithInvalideEmail extends BizBangladesh {
-	
-	JoinFreeSignIn invaild= new JoinFreeSignIn();
-	
-	@Test
-	 
-	 public void SignInTest(){
-		 System.out.println("Sign in with vaild email and password ");
-		 clickByXpath(invaild.clickSignIn);
-		 typeByXpath(invaild.emailLoc , invaild.invaildemailLoc);
-		 typeByXpath(invaild.passwordLoc, invaild.passwordvalue);
-		 clickByXpath(invaild.signInButtonLoc);
-		 
-		 String act = driver.findElement(By.xpath(invaild.myActualFullNameLoc)).getText(); 
-		 String exp = "Mohammad uddin";
-		 Assert.assertEquals(act, exp);
-}
->>>>>>> 922ffcbd5f7dc18cd9a93f6a1af56edff6912587
 }
